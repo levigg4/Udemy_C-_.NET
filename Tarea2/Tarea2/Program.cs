b15fff00fd7fff29897d791ead9e66c8755a13d7
Levi Garcia <58457288+levigg4@users.noreply.github.com>
2023-02-26 17:58:26 -0600

    Add files via upload

---
+++ Tarea2/Tarea2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Calcular perimetro poligono regular
+            double lado,peri;
+            int n;
+            Console.Write("Dame el numero de lados: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Dame el tamaño del lado: ");
+            lado = Convert.ToDouble(Console.ReadLine());
+            peri = n*lado; //formula de perimetro de poligonos regulares
+            Console.WriteLine("El perimetro es: {0}", peri);
+
+            ///conversor de centigrados a fahrenheit
+            double cel, fah;
+            Console.Write("Dame los grados Celsius: ");
+            cel = Convert.ToDouble(Console.ReadLine());
+            fah = (cel * 1.8)+32; //formula de celcius a fahrenheit
+            Console.WriteLine("{0}° Celsius son {1}F Fahrenheit",cel,fah);
+        }
+    }
+}
