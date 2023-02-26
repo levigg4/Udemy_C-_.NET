55770cc2433a88f88b92c8c0eff58f27468fa5b0
Levi Garcia <58457288+levigg4@users.noreply.github.com>
2023-02-26 15:25:03 -0600

    Add files via upload

---
+++ Tarea1/Tarea1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rodrigo Levi Garcia Garay");
+            Console.WriteLine("México");
+            Console.WriteLine("Juego Basquetbol");
+        }
+    }
+}
